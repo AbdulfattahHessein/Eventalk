8fa61f90cd881e1e4d5210dd601023834f4e40cf
Abdulfattah Hessein <abdulfattah.hessein@gmail.com>
2024-07-26 01:41:09 +0300

    fix sequelize issues with WebPack

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,8 +1,11 @@
 const dbConfig = require("../config/dbConfig");
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2"); // Needed to fix sequelize issues with WebPack
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
   define: {
     timestamps: false,
   },
